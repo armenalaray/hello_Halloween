84e1e994d2de54e54ec838fd3e20d5023cbec89b
Alejandro Armenta <armenalaray@gmail.com>
2017-03-24 14:33:42 -0600

    Set Animations
    Se cargaron animaciones default que encontre en unity

--- Halloween_Main_Project/Assets/Scripts/movement.cs
+++ Halloween_Main_Project/Assets/Scripts/movement.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 public class movement : MonoBehaviour {
 
 	public float vel;
-	public GameObject _camera;
-	public Transform objetivo;
-	public float suavidad;
+	//public GameObject _camera;
+	//public Transform objetivo;
+	//public float suavidad;
 	//public Quaternion objRot;
-	Vector3 compensar;
+	//Vector3 compensar;
 	// Use this for initialization
 	void Start () {
-		compensar = _camera.transform.position - this.transform.position;//distancia entre oobjetivo y camara
+		//compensar = _camera.transform.position - this.transform.position;//distancia entre oobjetivo y camara
 		//objRot = _camera.transform.rotation;
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//movimiento del jugador
-		transform.position += new Vector3 (Input.GetAxis("Horizontal") * vel*Time.deltaTime,0,Input.GetAxis("Vertical")*vel*Time.deltaTime); 
+		transform.position += new Vector3 (Input.GetAxis("Horizontal") * vel*Time.deltaTime,Input.GetAxis("Vertical")*vel*Time.deltaTime, 0); 
 
 		if (Input.GetKey (KeyCode.Space)) {
 					
@@ -27,12 +28,12 @@ public class movement : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
-		
-		Vector3 objetivoCam = transform.position + compensar;
-		_camera.transform.LookAt(transform);
+       
+		//Vector3 objetivoCam = this.transform.position + compensar;
+		//_camera.transform.LookAt(transform);
 		//objRot -= this.transform.rotation.SetFromToRotation (_camera.transform.position, objetivoCam); 
-		_camera.transform.position = Vector3.Lerp (_camera.transform.position, objetivoCam, suavidad * Time.deltaTime);
-
-		
-	}
+		//_camera.transform.position = Vector3.Lerp (_camera.transform.position, objetivoCam, suavidad * Time.deltaTime);
+        //_camera.transform.position = objetivoCam;
+        //Debug.Log(objetivoCam);
+    }
 }
