6c228038b9352059773103fb9c7dba1e840bf70c
ArcadeFan <paper_moon_4@hotmail.com>
2017-05-25 08:53:33 -0500

    Props Added
    Trees  and pumpkins

--- Halloween_Main_Project/Assets/Scripts/Wall.cs
+++ Halloween_Main_Project/Assets/Scripts/Wall.cs
@@ -1,12 +1,12 @@
-﻿using System.Collections;
+﻿    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Wall : MonoBehaviour {
 
-    public Sprite dmgSprite;
-    public int hp = 4;
-
+    public Sprite [] dmgSprite;
+    public int hp = 5;
+    private int count = 0;
     private SpriteRenderer spriteRenderer;
 	
     // Use this for initialization
@@ -16,12 +16,20 @@ public class Wall : MonoBehaviour {
 	
 	public void DamageWall(int loss)
     {
-        spriteRenderer.sprite = dmgSprite;
+        if (count % 2 == 1 && count < 5)
+        {
+            int index = count / 2;
+            spriteRenderer.sprite = dmgSprite[index];
+        }
+
         hp -= loss;
         if(hp <= 0)
         {
             //wall disapppears si su hp es menor a 0
             gameObject.SetActive(false);
         }
+        
+
+        count += 1;
     }
 }
